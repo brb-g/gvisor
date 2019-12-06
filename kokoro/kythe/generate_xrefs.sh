d27a9ac8bcadf1b8997bd20030cfe96d64fc72a9
Brad Burlage <brb@google.com>
2019-12-06 11:16:17 -0800

    Add generate_xrefs config

---
+++ kokoro/kythe/generate_xrefs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+readonly KYTHE_VERSION='v0.0.37'
+readonly WORKDIR="$(mktemp -d)"
+readonly KYTHE_DIR="${WORKDIR}/kythe-${KYTHE_VERSION}"
+readonly KZIP_FILENAME="$(git rev-parse HEAD).kzip"
+
+wget -q -O "${WORKDIR}/kythe.tar.gz" \
+  "https://github.com/kythe/kythe/releases/download/${KYTHE_VERSION}/kythe-${KYTHE_VERSION}.tar.gz"
+tar --no-same-owner -xvzf "${WORKDIR}/kythe.tar.gz" --directory "$WORKDIR"
+
+bazel \
+  --bazelrc="${KYTHE_DIR}/extractors.bazelrc" \
+  build \
+  --override_repository kythe_release="${KYTHE_DIR}" \
+  --define=kythe_corpus=gvisor.dev \
+  //...
+
+"${KYTHE_DIR}/tools/kzip" merge \
+  --output "$KZIP_FILENAME" \
+  $(find -L bazel-out/*/extra_actions/ -name '*.kzip')

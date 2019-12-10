99d2f7cde5b271caf5b496148ba06bb54e05225f
Brad Burlage <brb@google.com>
2019-12-10 11:14:56 -0800

    restore bazel invocation, try using python3 for pip

--- kokoro/kythe/generate_xrefs.sh
+++ kokoro/kythe/generate_xrefs.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -ex
 
-if which use_bazel.sh >/dev/null 2>/dev/null; then
+# Install the latest version of Bazel. The default on Kokoro images is out of
+# date.
+if command -v use_bazel.sh >/dev/null; then
   use_bazel.sh latest
 fi
 bazel version
 
+# We need to use python 3.6 (the Kokoro PY3 default is 3.4) to compile `//...`
+# because benchmarktools requires a version of `requests` that is not a
+# available in 3.4.
 pyenv versions
 pyenv global 3.6.1
 
 readonly KYTHE_VERSION='v0.0.37'
 readonly WORKDIR="$(mktemp -d)"
-
-wget -q -O "${WORKDIR}/kythe.tar.gz" \
-  "https://github.com/kythe/kythe/releases/download/${KYTHE_VERSION}/kythe-${KYTHE_VERSION}.tar.gz"
-tar --no-same-owner -xzf "${WORKDIR}/kythe.tar.gz" --directory "$WORKDIR"
 readonly KYTHE_DIR="${WORKDIR}/kythe-${KYTHE_VERSION}"
-
 if [[ -n "$KOKORO_ARTIFACTS_DIR" ]]; then
   cd "${KOKORO_ARTIFACTS_DIR}/github/gvisor"
 fi
@@ -25,14 +25,18 @@ if [[ -n "$KOKORO_GIT_COMMIT" ]]; then
 else
   readonly KZIP_FILENAME="$(git rev-parse HEAD).kzip"
 fi
-#bazel \
-#  --bazelrc="${KYTHE_DIR}/extractors.bazelrc" \
-#  build \
-#  --override_repository kythe_release="${KYTHE_DIR}" \
-#  --define=kythe_corpus=gvisor.dev \
-#  //...
 
-#"${KYTHE_DIR}/tools/kzip" merge \
-#  --output "$KZIP_FILENAME" \
-#  $(find -L bazel-out/*/extra_actions/ -name '*.kzip')
-echo foo > "$KZIP_FILENAME"
+wget -q -O "${WORKDIR}/kythe.tar.gz" \
+  "https://github.com/kythe/kythe/releases/download/${KYTHE_VERSION}/kythe-${KYTHE_VERSION}.tar.gz"
+tar --no-same-owner -xzf "${WORKDIR}/kythe.tar.gz" --directory "$WORKDIR"
+
+bazel \
+  --bazelrc="${KYTHE_DIR}/extractors.bazelrc" \
+  build \
+  --override_repository kythe_release="${KYTHE_DIR}" \
+  --define=kythe_corpus=gvisor.dev \
+  //...
+
+"${KYTHE_DIR}/tools/kzip" merge \
+  --output "$KZIP_FILENAME" \
+  $(find -L bazel-out/*/extra_actions/ -name '*.kzip')
